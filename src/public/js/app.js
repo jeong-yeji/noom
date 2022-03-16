0124f89f60322b0dad18e0f5b1d599020ef8ff5a
jeong-yeji <jeongyeji.mail@gmail.com>
2022-03-17 00:26:56 +0900

    2.4 Rooms

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2,9 +2,17 @@ const socket = io();
 
 const welcome = document.getElementById('welcome');
 const form = welcome.querySelector('form');
+const room = document.getElementById('room');
 
-function backendDone(msg) {
-    console.log(`The backend says: `, msg);
+room.hidden = true;
+
+let roomName;
+
+function showRoom() {
+    welcome.hidden = true;
+    room.hidden = false;
+    const h3 = room.querySelector('h3');
+    h3.innerText = `Room ${roomName}`;
 }
 
 function handleRoomSubmit(event) {
@@ -13,7 +21,8 @@ function handleRoomSubmit(event) {
     // arg1 : event name - it can be anything. we can send any event that we make. => can custom event name
     // argsN : payload - we can send ANYTHING(json, string, boolean, number...). do not need to change object to string. we can send AS MANY AS we want.
     // last args : func that we can call from SERVER - call at BE, but func is on FE
-    socket.emit('enter_room', input.value, backendDone); // := socket.send()
+    socket.emit('enter_room', input.value, showRoom); // := socket.send()
+    roomName = input.value;
     input.value = '';
 }
 form.addEventListener('submit', handleRoomSubmit);
