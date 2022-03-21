fee6396f1753398f928328dab15ac3304ae3c360
jeong-yeji <jeongyeji.mail@gmail.com>
2022-03-21 21:08:02 +0900

    3.0 User Video

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,90 +1,44 @@
 const socket = io();
 
-const welcome = document.getElementById('welcome');
-const form = welcome.querySelector('form');
-const room = document.getElementById('room');
+const myFace = document.getElementById('myFace');
+const muteBtn = document.getElementById('mute');
+const cameraBtn = document.getElementById('camera');
 
-room.hidden = true;
+let myStream;
+let muted = false;
+let cameraOff = false;
 
-let roomName;
-
-function addMessage(message) {
-    const ul = room.querySelector('ul');
-    const li = document.createElement('li');
-    li.innerText = message;
-    ul.appendChild(li);
-}
-
-function handleMessageSubmit(event) {
-    event.preventDefault();
-    const input = room.querySelector('#msg input');
-    const value = input.value;
-    socket.emit('new_message', input.value, roomName, () => {
-        addMessage(`You: ${value}`);
-    });
-    input.value = '';
-}
-
-function handleNicknameSubmit(event) {
-    event.preventDefault();
-    const input = room.querySelector('#name input');
-    socket.emit('nickname', input.value);
-}
-
-function refreshTitle(newCount) {
-    const h3 = room.querySelector('h3');
-    h3.innerText = `Room ${roomName} (${newCount})`;
-}
-
-function showRoom(roomCount) {
-    welcome.hidden = true;
-    room.hidden = false;
-    refreshTitle(roomCount);
-    const msgForm = room.querySelector('#msg');
-    const nameForm = room.querySelector('#name');
-    msgForm.addEventListener('submit', handleMessageSubmit);
-    nameForm.addEventListener('submit', handleNicknameSubmit);
-}
-
-function handleRoomSubmit(event) {
-    event.preventDefault();
-
-    // const input = form.querySelector('input');
-    const room_name = form.querySelector('#roomName');
-    const name = form.querySelector('#name');
-
-    // arg1 : event name - it can be anything. we can send any event that we make. => can custom event name
-    // argsN : payload - we can send ANYTHING(json, string, boolean, number...). do not need to change object to string. we can send AS MANY AS we want.
-    // last args : func that we can call from SERVER - call at BE, but func is on FE
-    socket.emit('enter_room', room_name.value, name.value, showRoom); // := socket.send()
-    roomName = room_name.value;
-    room_name.value = '';
-
-    room.querySelector('#name input').value = name.value;
-}
-form.addEventListener('submit', handleRoomSubmit);
-
-socket.on('welcome', (user, newCount) => {
-    refreshTitle(newCount);
-    addMessage(`${user} joined!`);
-});
-
-socket.on('bye', (left, newCount) => {
-    refreshTitle(newCount);
-    addMessage(`${left} left!`);
-});
-
-socket.on('new_message', addMessage);
-
-socket.on('room_change', (rooms) => {
-    const roomList = welcome.querySelector('ul');
-    roomList.innerHTML = '';
-    if (rooms.length === 0) {
-        return;
+async function getMedia() {
+    try {
+        myStream = await navigator.mediaDevices.getUserMedia({
+            audio: true,
+            video: true,
+        });
+        myFace.srcObject = myStream;
+    } catch (e) {
+        console.log(e);
     }
-    rooms.forEach((room) => {
-        const li = document.createElement('li');
-        li.innerText = room;
-        roomList.append(li);
-    });
-});
+}
+getMedia();
+
+function handleMuteClick() {
+    if (!muted) {
+        muteBtn.innerText = 'Unmute';
+        muted = true;
+    } else {
+        muteBtn.innerText = 'Mute';
+        muted = false;
+    }
+}
+function handleCameraClick() {
+    if (cameraOff) {
+        cameraBtn.innerText = 'Turn Camera Off';
+        cameraOff = false;
+    } else {
+        cameraBtn.innerText = 'Turn Camera On';
+        cameraOff = true;
+    }
+}
+
+muteBtn.addEventListener('click', handleMuteClick);
+cameraBtn.addEventListener('click', handleCameraClick);
--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 import http from 'http';
-import { Server } from 'socket.io';
-import { instrument } from '@socket.io/admin-ui';
+import SocketIO from 'socket.io';
 import express from 'express';
 
 const app = express();
@@ -14,102 +13,7 @@ app.get('/*', (_, res) => res.redirect('/')); // catchall url
 // http server
 const httpServer = http.createServer(app);
 // IO(Web Socket) Server
-const wsServer = new Server(httpServer, {
-    cors: {
-        origin: ['https://admin.socket.io'],
-        credentials: true,
-    },
-});
-
-instrument(wsServer, {
-    auth: false,
-});
-
-function publicRooms() {
-    const {
-        sockets: {
-            adapter: { sids, rooms },
-        },
-    } = wsServer; // same as following codes
-    // const sids = wsServer.sockets.adapters.sids;
-    // const rooms = wsServer.sockets.adapters.rooms;
-
-    const publicRooms = [];
-    rooms.forEach((_, key) => {
-        if (sids.get(key) === undefined) {
-            publicRooms.push(key);
-        }
-    });
-
-    return publicRooms;
-}
-
-function countRoom(roomName) {
-    return wsServer.sockets.adapter.rooms.get(roomName)?.size;
-}
-
-wsServer.on('connection', (socket) => {
-    socket['nickname'] = 'Annoymous';
-    wsServer.sockets.emit('room_change', publicRooms());
-
-    socket.onAny((event) => {
-        console.log(`Socket Event: ${event}`);
-    });
-
-    // get room name >> 10 secs later >> call done() from BE & execute done() on FE
-    socket.on('enter_room', (roomName, nickname, done) => {
-        socket['nickname'] = nickname;
-        // In Socket.io, all sockets(users) have a private room between them and server.
-        socket.join(roomName);
-        const cnt = countRoom(roomName);
-        done(cnt);
-        socket.to(roomName).emit('welcome', socket.nickname, cnt);
-        wsServer.sockets.emit('room_change', publicRooms());
-    });
-
-    // disconnecting : the client is going to be disconnected, but hasn't left yet
-    socket.on('disconnecting', () => {
-        socket.rooms.forEach((room) => {
-            socket.to(room).emit('bye', socket.nickname, countRoom(room) - 1);
-        });
-    });
-
-    socket.on('disconnect', () => {
-        wsServer.sockets.emit('room_change', publicRooms());
-    });
-
-    socket.on('new_message', (msg, room, done) => {
-        socket.to(room).emit('new_message', `${socket.nickname}: ${msg}`);
-        done();
-    });
-
-    socket.on('nickname', (nickname) => (socket['nickname'] = nickname));
-});
-
-/*
-// web socket server
-const wss = new WebSocketServer({ server });
-
-const sockets = [];
-
-// the socket of server.js represents connected browser
-// use annoymous function
-wss.on('connection', (socket) => {
-    sockets.push(socket);
-    socket['nickname'] = 'Anonymous';
-    console.log('Connected to Browser ✔');
-    socket.on('close', () => console.log('Disconnected from the Browser ❌'));
-    socket.on('message', (msg) => {
-        const message = JSON.parse(msg); // from String to JS object
-        switch (message.type) {
-            case 'new_message':
-                sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));
-                case 'nickname':
-                    socket['nickname'] = message.payload;
-                }
-    });
-});
-*/
+const wsServer = SocketIO(httpServer);
 
 const handleListen = () => console.log('Listening on http://localhost:3000');
 httpServer.listen(3000, handleListen);
