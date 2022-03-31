c5bcfb127cba306982a86de885c9cb7fac639571
jeong-yeji <jeongyeji.mail@gmail.com>
2022-03-31 14:18:56 +0900

    3.4 Rooms

--- src/server.js
+++ src/server.js
@@ -15,5 +15,13 @@ const httpServer = http.createServer(app);
 // IO(Web Socket) Server
 const wsServer = SocketIO(httpServer);
 
+wsServer.on('connection', (socket) => {
+    socket.on('join_room', (roomName, done) => {
+        socket.join(roomName);
+        done();
+        socket.to(roomName).emit('welcome');
+    });
+});
+
 const handleListen = () => console.log('Listening on http://localhost:3000');
 httpServer.listen(3000, handleListen);
